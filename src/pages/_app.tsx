75eb98fbcb8bd4d7864bb43f823700f382597085
duclm <ducle7144@gmail.com>
2022-09-24 16:38:25 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,41 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
+import { Spin } from "antd"
 import { AppProps } from "next/app"
-
 import "../styles/tailwind.scss"
 import "antd/dist/antd.css"
+import { useRouter } from "next/router"
+
 import AppLayout from "@components/Layout/Layout"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const handleStart = (url: string) =>
+      url !== router.asPath && setLoading(true)
+    const handleComplete = (url: string) =>
+      url === router.asPath && setLoading(false)
+
+    router.events.on("routeChangeStart", handleStart)
+    router.events.on("routeChangeComplete", handleComplete)
+    router.events.on("routeChangeError", handleComplete)
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart)
+      router.events.off("routeChangeComplete", handleComplete)
+      router.events.off("routeChangeError", handleComplete)
+    }
+  })
   return (
     <AppLayout>
-      <Component {...pageProps} />
+      {loading ? (
+        <div className="absolute left-1/2 top-1/2 flex items-center justify-center">
+          <Spin />
+        </div>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </AppLayout>
   )
 }
